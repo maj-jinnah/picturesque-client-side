cf9e9a5e41187fa51737ac7f0055595c1bef7a5b
maj-jinnah <maj.jinnah@gmail.com>
2022-11-08 14:40:30 +0600

    footer added

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,17 +5,20 @@ const Blog = () => {
         <div>
             <div className='my-8 mx-8'>
                 <div className='mb-8'>
-                    <h1 className='text-2xl font-bold'>Question 1:  what is cors?</h1>
-                    <p>Answer: CORS stands for Cross-Origin Resource Sharing. It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API. In other words, CORS is a browser security feature that restricts cross-origin HTTP requests with other servers and specifies which domains access your resources.</p>
+                    <h1 className='text-2xl font-bold'>Question 1:  Difference between SQL and NoSQL?</h1>
+                    <p><span className='font-bold'> Answer:</span> Structured Query language "SQL" pronounced as “S-Q-L” or sometimes as "See-Quel" is the standard language for dealing with Relational Databases. A relational database defines relationships in the form of tables. SQL programming can be effectively used to insert, search, update, delete database records. That doesn't mean SQL cannot do things beyond that. It can do a lot of things including, but not limited to, optimizing and maintenance of databases. <br />
+                        NoSQL is a non-relational DMS, that does not require a fixed schema, avoids joins, and is easy to scale. NoSQL database is used for distributed data stores with humongous data storage needs. NoSQL is used for Big data and real-time web apps. NoSQL database stands for “Not Only SQL” or “Not SQL.” Though a better term would NoREL NoSQL caught on. Traditional RDBMS uses SQL syntax to store and retrieve data for further insights. Instead, a NoSQL database system encompasses a wide range of database technologies that can store structured, semi-structured, unstructured and polymorphic data.
+
+                    </p>
                 </div>
                 <div className='mb-8'>
-                    <h1 className='text-2xl font-bold'>question 2: Why are you using firebase? What other options do you have to implement authentication?</h1>
-                    <p>Answer: Google Firebase is an application development platform that allows developers to create iOS, Android, and Web apps. When you build a client-side app with JavaScript or any of its frameworks, for instance, Google Firebase can turn this into a serverless app in no time. It also removes the need to manage databases yourself, as it does that for you. Therefore, implementing Firebase means plugging a ready-made backend into your client code to make it dynamic. Ultimately, it eliminates the need to write backend code from scratch and gives you a fully functional one instead. Security-wise, it also has explicitly built-in security rules that make it a trusted data and server handler. Plus, you get a protected backend when you use these rules. There are various ways through which we can achieve authentication. Such as,</p>
-                    <p>1. Cookie-Based authentication</p>
-                    <p>2. Token-Based authentication</p>
-                    <p>3. Third party access(OAuth, API-token)</p>
-                    <p>4. OpenId</p>
-                    <p>5. SAML</p>
+                    <h1 className='text-2xl font-bold'>question 2: What is JWT, and how does it work?</h1>
+                    <p><span className='font-bold'> Answer:</span> JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.<br/>
+                        JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted. A JWT is a string made up of three parts, separated by dots ".", and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
+                    </p>
+                    <p>Once decoded, you will get two JSON strings:</p>
+                    <p>1. The header and the payload.</p>
+                    <p>2. The signature.</p>
                 </div>
                 <div className='mb-8'>
                     <h1 className='text-2xl font-bold'>Question 3: How does the private route work?</h1>
